05ca53d57e80b7938eb6706158384c62af61aab0
Mike Bunton <mr.bunton@Mikes-MacBook-Air.local>
2022-12-06 23:13:45 -0600

    Started the blog

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Navigation from './components/navigation';
+import {BrowserRouter as Router, Routes, Route, redirect} from 'react-router-dom'
+import PageRenderer from './page-renderer';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navigation/>
+      <Routes>
+        <Route path="/:page" component={PageRenderer}/>
+        <Route path="/" render={() => <redirect to="/home" />} />
+        <Route component={() => 404}/>
+      </Routes>
     </div>
+    </Router>
   );
 }
 
